cfe28c04b84c087d0d4d52366b00551d1ebcb42b
KRIS2604 <krissathwara2604@gmail.com>
2026-04-03 20:21:19 -0500

    Little changes

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from models import db, Product, CartItem, Order, OrderItem
 
 
 def create_app():
-    app = Flask(__name__, template_folder="templates", static_folder="static")
+    app = Flask(__name__, template_folder="template", static_folder="static")
     app.config.from_object(Config)
 
     CORS(app)
